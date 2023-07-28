be4e823031a3ba08eb4b155560cc1255989fe240
wallenjos01 <unconfigured@null.spigotmc.org>
2023-07-28 11:12:17 -0600

    Document session module, prevent a Player from joining multiple registered Sessions

--- server/src/main/java/org/wallentines/mcore/session/SessionModule.java
+++ server/src/main/java/org/wallentines/mcore/session/SessionModule.java
@@ -27,7 +27,6 @@ import java.util.stream.Stream;
 public abstract class SessionModule implements ServerModule {
 
     protected Singleton<Server> server;
-
     private final HashMap<UUID, Session> runningSessions = new HashMap<>();
 
     @Override
@@ -38,21 +37,44 @@ public abstract class SessionModule implements ServerModule {
         return true;
     }
 
+    /**
+     * Creates and registers a new session with the given factory. Unregistered sessions should just call the Factory's
+     * create method directly
+     * @param namespace The namespace for the new session
+     * @param factory The factory by which to create a new session
+     * @return A new registered session
+     * @param <T> The type of session to create
+     */
     public <T extends Session> T createSession(String namespace, Session.Factory<T> factory) {
         T out = factory.create(this, server.get(), namespace);
         runningSessions.put(out.getId(), out);
+        out.isRegistered = true;
         out.shutdownEvent.register(this, sess -> runningSessions.remove(sess.getId()));
         return out;
     }
 
+    /**
+     * Gets a list of registered Sessions
+     * @return A list of registered sessions
+     */
     public Collection<Session> getSessions() {
         return runningSessions.values();
     }
 
+    /**
+     * Gets a stream of registered Sessions that match the given predicate
+     * @param predicate The predicate to check Sessions by
+     * @return A stream of registered Sessions
+     */
     public Stream<Session> getSessions(Predicate<Session> predicate) {
         return runningSessions.values().stream().filter(predicate);
     }
 
+    /**
+     * Gets the registered Session the given player is in
+     * @param player The player to search for
+     * @return The registered Session that player is in, or null if they aren't in a Session
+     */
     public Session getPlayerSession(Player player) {
         for(Session sess : runningSessions.values()) {
             if(sess.contains(player)) return sess;
@@ -60,31 +82,42 @@ public abstract class SessionModule implements ServerModule {
         return null;
     }
 
+    /**
+     * Shuts down all registered Sessions
+     */
     public void shutdownAll() {
 
         List.copyOf(runningSessions.values()).forEach(Session::shutdown);
     }
 
+    /**
+     * Shuts down all registered Sessions which match the given predicate
+     * @param predicate The predicate to check Sessions by
+     */
     public void shutdownAll(Predicate<Session> predicate) {
 
         runningSessions.values().stream().filter(predicate).toList().forEach(Session::shutdown);
     }
 
-
+    /**
+     * Attempts to recover player save points when they join the server. Should be called each time a player joins the
+     * server
+     * @param player The player to attempt recovery for
+     */
     protected void attemptRecovery(Player player) {
 
-        File lookup = server.get().getStorageDirectory().resolve(MidnightCoreAPI.MOD_ID + "-session-recovery-" + player.getUUID().toString() + ".json").toFile();
+        File lookup = getRecoveryFile(player);
+        if(!lookup.isFile()) return;
         new FileExecutor(lookup, (file) -> {
-            if (!lookup.isFile()) return;
 
             SavepointModule module = server.get().getModuleManager().getModule(SavepointModule.class);
             SerializeResult<Savepoint> savepoint = module.getSerializer().deserialize(
                     ConfigContext.INSTANCE,
-                    JSONCodec.fileCodec().loadFromFile(ConfigContext.INSTANCE, lookup, StandardCharsets.UTF_8));
+                    JSONCodec.fileCodec().loadFromFile(ConfigContext.INSTANCE, file, StandardCharsets.UTF_8));
 
             if(savepoint.isComplete()) {
                 savepoint.getOrThrow().load(player);
-                if(!lookup.delete()) {
+                if(!file.delete()) {
                     MidnightCoreAPI.LOGGER.warn("Failed to delete session recovery file " + file.getAbsolutePath() + "! Please delete it manually, or else the session module may attempt to restore it again!");
                 }
             } else {
@@ -92,6 +125,17 @@ public abstract class SessionModule implements ServerModule {
             }
         }).start();
 
+    }
+
+    /**
+     * Gets the recovery file name for the given Player
+     * @param player The player to lookup
+     * @return The recovery file name
+     */
+    public static File getRecoveryFile(Player player) {
+
+        String name = MidnightCoreAPI.MOD_ID + "-recovery-" + player.getUUID().toString() + ".tmp";
+        return player.getServer().getStorageDirectory().resolve(name).toFile();
     }
 
     public static final Identifier ID = new Identifier(MidnightCoreAPI.MOD_ID, "session");
