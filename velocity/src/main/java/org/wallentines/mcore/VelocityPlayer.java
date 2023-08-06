e8fe36f9cc8cfb36fc1f09dc82c462ccabe41d4a
M1dnightNinja <wallenjos01@gmail.com>
2023-08-06 12:38:54 -0600

    Implement hacky message sending

--- velocity/src/main/java/org/wallentines/mcore/VelocityPlayer.java
+++ velocity/src/main/java/org/wallentines/mcore/VelocityPlayer.java
@@ -1,8 +1,12 @@
 package org.wallentines.mcore;
 
+import com.google.gson.JsonObject;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.wallentines.mcore.text.Component;
+import org.wallentines.mcore.text.ModernSerializer;
+import org.wallentines.mcore.util.GsonContext;
 
 import java.util.UUID;
 
@@ -26,7 +30,11 @@ public class VelocityPlayer implements ProxyPlayer {
 
     @Override
     public void sendMessage(Component message) {
-        // TODO: Converting components to adventure components
+        // TODO: Proper conversion of components to adventure components
+
+        JsonObject obj = ModernSerializer.INSTANCE.serialize(GsonContext.INSTANCE, message).getOrThrow().getAsJsonObject();
+        net.kyori.adventure.text.Component cmp = GsonComponentSerializer.builder().build().deserializeFromTree(obj);
+        player.sendMessage(cmp);
     }
 
     @Override
