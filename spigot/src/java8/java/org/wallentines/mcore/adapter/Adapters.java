1be6c80ad59e6c1ec4c63182f132929386595a6a
M1dnightNinja <wallenjos01@gmail.com>
2023-08-26 11:10:16 -0600

    Add 1.9 adapters

--- spigot/src/java8/java/org/wallentines/mcore/adapter/Adapters.java
+++ spigot/src/java8/java/org/wallentines/mcore/adapter/Adapters.java
@@ -12,6 +12,10 @@ public class Adapters {
                 return new org.wallentines.mcore.adapter.v1_8_R1.AdapterImpl();
             case "v1_8_R2":
                 return new org.wallentines.mcore.adapter.v1_8_R2.AdapterImpl();
+            case "v1_9_R1":
+                return new org.wallentines.mcore.adapter.v1_9_R1.AdapterImpl();
+            case "v1_9_R2":
+                return new org.wallentines.mcore.adapter.v1_9_R2.AdapterImpl();
             case "v1_8_R3":
                 return new org.wallentines.mcore.adapter.v1_8_R3.AdapterImpl();
             case "v1_16_R3":
