ab1694fe95f315b8386a855096fd665a147c7961
wallenjos01 <wallenjos01@gmail.com>
2024-05-15 10:13:19 -0600

    Allow sending unresolved components to CommandSender

--- server/src/main/java/org/wallentines/mcore/CommandSender.java
+++ server/src/main/java/org/wallentines/mcore/CommandSender.java
@@ -1,13 +1,22 @@
 package org.wallentines.mcore;
 
 import org.wallentines.mcore.lang.LocaleHolder;
+import org.wallentines.mcore.lang.UnresolvedComponent;
 import org.wallentines.mcore.text.Component;
 
 public interface CommandSender extends PermissionHolder, LocaleHolder {
 
     void sendSuccess(Component component, boolean log);
 
+    default void sendSuccess(UnresolvedComponent component, boolean log) {
+        sendSuccess(component.resolveFor(this), log);
+    }
+
     void sendFailure(Component component);
+
+    default void sendFailure(UnresolvedComponent component) {
+        sendFailure(component.resolveFor(this));
+    }
 
     Location getLocation();
 
