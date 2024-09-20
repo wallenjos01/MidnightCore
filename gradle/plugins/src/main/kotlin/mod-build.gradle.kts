351fdca1a18617df36aeeee0c53ea3e06d8266c9
wallenjos01 <wallenjos01@gmail.com>
2024-09-19 19:04:43 -0600

    Pre-Release 8

--- gradle/plugins/src/main/kotlin/mod-build.gradle.kts
+++ gradle/plugins/src/main/kotlin/mod-build.gradle.kts
@@ -22,8 +22,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
 }
 
 tasks.withType<Jar> {
