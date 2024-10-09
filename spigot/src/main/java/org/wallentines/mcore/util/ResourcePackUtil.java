df95de67f39715f05e19239353c3528beef6332b
wallenjos01 <wallenjos01@gmail.com>
2024-10-09 15:18:12 -0600

    Properly send resource pack hash on Spigot

--- spigot/src/main/java/org/wallentines/mcore/util/ResourcePackUtil.java
+++ spigot/src/main/java/org/wallentines/mcore/util/ResourcePackUtil.java
@@ -10,6 +10,19 @@ import java.util.UUID;
 
 public class ResourcePackUtil {
 
+    public static byte[] readHash(String str) {
+
+        byte[] out = new byte[str.length() / 2];
+        try {
+            for (int i = 0; i < out.length; i++) {
+                out[i] = (byte) Integer.parseInt(str.substring(i * 2, i * 2 + 2), 16);
+            }
+        } catch (NumberFormatException ex) {
+            return null;
+        }
+        return out;
+    }
+
     public static void addResourcePack(SpigotPlayer player, ResourcePack pack) {
 
         PlaceholderContext ctx = new PlaceholderContext();
@@ -18,15 +31,18 @@ public class ResourcePackUtil {
         UnresolvedComponent msg = pack.message();
         String message = msg == null ? null : msg.resolve(ctx).toLegacyText();
 
+        byte[] hash = readHash(pack.hash());
+
         GameVersion version = player.getServer().getVersion();
         if(version.hasFeature(GameVersion.Feature.RESOURCE_PACK_IDS)) {
-            player.getInternal().addResourcePack(pack.uuid(), pack.url(), pack.hash().getBytes(), message, pack.forced());
+            player.getInternal().addResourcePack(pack.uuid(), pack.url(), hash, message, pack.forced());
 
             // Although pack message support was added in 1.17, the Bukkit API didn't support it until 1.18 (PV757)
         } else if(version.getProtocolVersion() >= 757) {
-            player.getInternal().setResourcePack(pack.url(), pack.hash().getBytes(), message, pack.forced());
+            player.getInternal().setResourcePack(pack.url(), hash, message, pack.forced());
+
         } else {
-            player.getInternal().setResourcePack(pack.url(), pack.hash().getBytes());
+            player.getInternal().setResourcePack(pack.url(), hash);
         }
     }
 
